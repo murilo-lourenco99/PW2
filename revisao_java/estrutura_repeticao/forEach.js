229796ad9cea9883daebeecf686fdee1441c37ce
Murilo Lourenço de Jesus <muriloljesus22@gmail.com>
2026-02-25 18:44:45 -0300

    Add files via upload

---
+++ revisao_java/estrutura_repeticao/forEach.js
@@ -0,0 +1,16 @@
+/* For Each:
+    Uso de ForEach como vetor para identificar as cores e criar uma lista de presença
+*/
+
+let cores = ["Azul", "Dourado", "Vermelho"];
+
+cores.forEach(function(cor) {
+  console.log(cor);
+});
+
+// lista de presença
+let alunos = ["murilo", "Mica", "Raul", "Estefani", "Emilly"];
+
+alunos.forEach(function(aluno) {
+  console.log(aluno, ": presente");
+});
--- revisao_java/estrutura_repeticao/map.js
+++ revisao_java/estrutura_repeticao/map.js
@@ -1,19 +1,19 @@
-// código que converte horas em minutos
-const horas = [2, 5, 8];
+/* Map
+    Uso de map como vetor copiador para fazer o cubo de números e conversão de horas em minutos
+*/
+
+let numeros = [10, 5, 8, 9, 2];
+
+let cubos = numeros.map(function(numero){
+    return numero*numero*numero;
+}) 
+
+console.log(cubos);
+
+// conversão de horas em minutos
+let horas = [2, 5, 8];
 console.log(horas);
 
 let minutos = horas.map(function(hora) {
     return hora * 60;
 })
-
-console.log(minutos);
-
-// código que converte letras minusculas pra maiúsculas
-const nomesPituchos = ["morim", "raulzito", "micarinha"];
-console.log(nomesPituchos);
-
-const nomesAltos = nomesPituchos.map(function(nome) {
-    return nome.toUpperCase();
-})
-
-console.log(nomesAltos);
